ac2005a3a3f926bbdbfd21b6d63d037eff356605
David Hession <L00069108@student.lyit.ie>
2018-11-14 21:04:45 +0000

    Added preliminary class structures for Pupil and Guardian.

---
+++ AbsenceMonitor/SchoolAbsenceMonitorUI/Guardian.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAbsenceMonitorUI
+{
+    class Guardian
+    {
+        public Guardian(string givenName, string surname, string mobileNo)
+        {
+            this.GivenName = givenName;
+            this.Surname = surname;
+            this.MobileNo = mobileNo;
+        }
+
+        public Guardian(string givenName, string surname, string mobileNo, string emergencyNo, string address )
+        {
+            this.GivenName = givenName;
+            this.Surname = surname;
+            this.MobileNo = mobileNo;
+        }
+
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+        public string MobileNo { get; set; }
+        public string Address { get; set; }
+        public string EmergencyNo { get; set; }
+        
+    }
+}
