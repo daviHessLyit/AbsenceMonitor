5802d146131f39a771d01314dfaf01595cdcd8f8
David Hession <L00069108@student.lyit.ie>
2018-11-25 15:26:35 +0000

    Committing Changes to admin page. Problems with some of the page load items.

--- AbsenceMonitor/SMAClassLibrary/SystemUserUtils.cs
+++ AbsenceMonitor/SMAClassLibrary/SystemUserUtils.cs
@@ -10,6 +10,7 @@ namespace SMAClassLibrary
     {
         SMADBEntities smaDB = new SMADBEntities("metadata = res://*/SchoolAbsenceMonitorModel.csdl|res://*/SchoolAbsenceMonitorModel.ssdl|res://*/SchoolAbsenceMonitorModel.msl;provider=System.Data.SqlClient;provider connection string='data source=DBSERVER;initial catalog=SMA_DB;persist security info=True;user id=davihess;password=d4vidH355;pooling=False;MultipleActiveResultSets=True;App=EntityFramework'");
 
+        // Add a new user to the database
         public void AddSystemUser(SystemUser systemUser)
         {
             // Check to see if the userName exists in the SystemDb first - if not add the new system user.
@@ -23,19 +24,46 @@ namespace SMAClassLibrary
             {
                 // Add the system user to the DataBase
                 smaDB.SystemUsers.Add(systemUser);
+                // Save the changes to the database
                 smaDB.SaveChanges();
             }
           
 
         }
 
+        // Method deletes the selected user from the database
         public void DeleteSystemUser(int systemUserId)
         {
             SystemUser systemUser = smaDB.SystemUsers.FirstOrDefault(s => s.UserId == systemUserId);
-            // Add the system user to the DataBase
+            // Remove the system user from the DataBase
             smaDB.SystemUsers.Remove(systemUser);
+            // Save the changes to the database
             smaDB.SaveChanges();
 
+        }
+
+        // Method updates an existing user on the database.
+        public void UpdateUserDetails(SystemUser systemUser)
+        {
+            SystemUser existingUserDetails = smaDB.SystemUsers.FirstOrDefault(s => s.UserId == systemUser.UserId);
+            // Update the user details
+            existingUserDetails.GivenName = systemUser.GivenName;
+            existingUserDetails.Surname = systemUser.Surname;
+            existingUserDetails.Password = systemUser.Password;
+            existingUserDetails.AccessLevelId = systemUser.AccessLevelId;
+            existingUserDetails.Username = systemUser.Username;
+
+            // Save the changes to the database
+            smaDB.SaveChanges();
+
+        }
+
+        // Method returns the selected user record from the data
+        public SystemUser GetUserDetails(int systemUserId)
+        {
+            SystemUser requestSystemUser = smaDB.SystemUsers.Where(s => s.UserId == systemUserId).FirstOrDefault();
+
+            return requestSystemUser;
         }
 
     }
--- AbsenceMonitor/SchoolAbsenceMonitorUI/Admin.xaml.cs
+++ AbsenceMonitor/SchoolAbsenceMonitorUI/Admin.xaml.cs
@@ -26,8 +26,7 @@ namespace SchoolAbsenceMonitorUI
 
         public Admin()
         {
-            InitializeComponent();
-            PopulateComboBox();
+            InitializeComponent();            
         }
 
         private void BtnReset_Click(object sender, RoutedEventArgs e)
@@ -49,12 +48,14 @@ namespace SchoolAbsenceMonitorUI
 
         private void BtnUserDelete_Click(object sender, RoutedEventArgs e)
         {
+            PopulateUserDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_SearchUser.Visibility = Visibility.Visible;
         }
 
         private void BtnUserUpdate_Click(object sender, RoutedEventArgs e)
         {
+            PopulateUserDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_SearchUser.Visibility = Visibility.Visible;
         }
@@ -67,12 +68,14 @@ namespace SchoolAbsenceMonitorUI
 
         private void BtnGuardDelete_Click(object sender, RoutedEventArgs e)
         {
+            PopulateGuardianDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_SearchGuardian.Visibility = Visibility.Visible;
         }
 
         private void BtnGuardUpdate_Click(object sender, RoutedEventArgs e)
         {
+            PopulateGuardianDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_SearchGuardian.Visibility = Visibility.Visible;
         }
@@ -91,12 +94,14 @@ namespace SchoolAbsenceMonitorUI
 
         private void BtnPupilUpdate_Click(object sender, RoutedEventArgs e)
         {
+            PopulatePupilDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_SearchPupil.Visibility = Visibility.Visible;
         }
 
         private void BtnPupilDelete_Click(object sender, RoutedEventArgs e)
         {
+            PopulatePupilDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_SearchPupil.Visibility = Visibility.Visible;
         }
@@ -109,12 +114,14 @@ namespace SchoolAbsenceMonitorUI
 
         private void BtnTeacherUpdate_Click(object sender, RoutedEventArgs e)
         {
+            PopulateTeacherDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_UpdateTeacher.Visibility = Visibility.Visible;
         }
 
         private void BtnTeacherDelete_Click(object sender, RoutedEventArgs e)
         {
+            PopulateTeacherDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_DeleteTeacher.Visibility = Visibility.Visible;
         }
@@ -133,24 +140,28 @@ namespace SchoolAbsenceMonitorUI
 
         private void BtnAbsenceUpdate_Click(object sender, RoutedEventArgs e)
         {
+            PopulateAbsenceDetails();
             Stk_UpdateAbsence.Visibility = Visibility.Visible;
             Stk_MenuPanel.Visibility = Visibility.Hidden;
         }
 
         private void BtnAbsenceDelete_Click(object sender, RoutedEventArgs e)
         {
+            PopulateAbsenceDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_DeleteAbsence.Visibility = Visibility.Visible;
         }
 
         private void BtnClassUpdate_Click(object sender, RoutedEventArgs e)
         {
+            PopulateClassDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_UpdateClass.Visibility = Visibility.Visible;
         }
 
         private void BtnClassDelete_Click(object sender, RoutedEventArgs e)
         {
+            PopulateClassDetails();
             Stk_MenuPanel.Visibility = Visibility.Hidden;
             Stk_DeleteClass.Visibility = Visibility.Visible;
         }
@@ -177,14 +188,79 @@ namespace SchoolAbsenceMonitorUI
         private void MnuIUpdateUser_Click(object sender, RoutedEventArgs e)
         {
             Stk_SearchUser.Visibility = Visibility.Hidden;
+            // Get the selected user details from the data base
+            var selectedUser = userUtils.GetUserDetails(Convert.ToInt16(LstUserSearch.SelectedValue.ToString()));
+            // Populate the user update details form.
+            TbxUpdateUserGiven.Text = selectedUser.GivenName.ToString();
+            TbxUpdateUserSurname.Text = selectedUser.Surname.ToString();
+            TbxUpdateUserName.Text = selectedUser.Username.ToString();
+            TbxUpdatePassword.Text = selectedUser.Password.ToString();
+            TbxUpdateUserID.Text = selectedUser.UserId.ToString();
+            // Make the form visible to the user.
             Stk_UpdateUserForm.Visibility = Visibility.Visible;
+        }
+
+        private void UpdateFormReload(int systemUserId)
+        {
+            var selectedUser = userUtils.GetUserDetails(systemUserId);
+            // Populate the user update details form.
+            TbxUpdateUserGiven.Text = selectedUser.GivenName.ToString();
+            TbxUpdateUserGiven.IsEnabled = false;
+            TbxUpdateUserSurname.Text = selectedUser.Surname.ToString();
+            TbxUpdateUserSurname.IsEnabled = false;
+            TbxUpdateUserName.Text = selectedUser.Username.ToString();
+            TbxUpdateUserName.IsEnabled = false;
+            TbxUpdatePassword.Text = selectedUser.Password.ToString();
+            TbxUpdatePassword.IsEnabled = false;
+            TbxUpdateUserID.Text = selectedUser.UserId.ToString();
+            LblAccessLevelDrp.Visibility = Visibility.Collapsed;
+            LblAccLvlId.Visibility = Visibility.Visible;
+            TbxUpdateAccessLevelId.Text = selectedUser.AccessLevelId.ToString();
+            TbxUpdateAccessLevelId.Visibility = Visibility.Visible;
+            CmbBxUpdateAdminLevel.Visibility = Visibility.Collapsed;
+            Lbl_UserUpdateSuccessLabel.Visibility = Visibility.Visible;
+            BtnUpdateUser.Visibility = Visibility.Collapsed;
+            BtnCancel.Visibility = Visibility.Collapsed;
+            BtnReturn.Visibility = Visibility.Visible;
+        }
+
+        private void UpdateFormReset()
+        {
+            TbxUpdateUserGiven.Clear();
+            TbxUpdateUserGiven.IsEnabled = true;
+            TbxUpdateUserSurname.Clear();
+            TbxUpdateUserSurname.IsEnabled = true;
+            TbxUpdateUserName.Clear();
+            TbxUpdateUserName.IsEnabled = true;
+            TbxUpdatePassword.Clear();
+            TbxUpdatePassword.IsEnabled = true;
+            TbxUpdateUserID.Clear();
+
+            LblAccLvlId.Visibility = Visibility.Hidden;
+            TbxUpdateAccessLevelId.Clear();
+            TbxUpdateAccessLevelId.Visibility = Visibility.Hidden;
+            LblAccessLevelDrp.Visibility = Visibility.Visible;
+            CmbBxUpdateAdminLevel.Visibility = Visibility.Visible;
+            Lbl_UserUpdateSuccessLabel.Visibility = Visibility.Hidden;           
+            BtnUpdateUser.Visibility = Visibility.Visible;
+            BtnCancel.Visibility = Visibility.Visible;
+            BtnReturn.Visibility = Visibility.Collapsed;
+
+            Stk_UpdateUserForm.Visibility = Visibility.Hidden;
         }
 
         private void MnuIDeleteUser_Click(object sender, RoutedEventArgs e)
         {
             Stk_SearchUser.Visibility = Visibility.Hidden;
+            // Get the selected user details from the data base
+            var selectedUser = userUtils.GetUserDetails(Convert.ToInt16(LstUserSearch.SelectedValue.ToString()));
+            // Populate the user details in the delete form
+            TbxDeleteUserGiven.Text = selectedUser.GivenName.ToString();
+            TbxDeleteUserSurname.Text = selectedUser.Surname.ToString();
+            TbxDeleteUserName.Text = selectedUser.Username.ToString();
+            TbxDeleteUserId.Text = selectedUser.UserId.ToString();
+            // Make the form visible to the user.
             Stk_DeleteUserForm.Visibility = Visibility.Visible;
-
         }
 
         private void MnuIUpdateGuardian_Click(object sender, RoutedEventArgs e)
@@ -228,7 +304,59 @@ namespace SchoolAbsenceMonitorUI
 
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private void PopulateUserDetails()
+        {
+            // Initialise a list of SystemUsers and fill with data if available
+            List<SystemUser> systemUsers = new List<SystemUser>();
+            LstUserSearch.ItemsSource = systemUsers;
+            foreach (var systemUser in smaDB.SystemUsers)
+            {
+                systemUsers.Add(systemUser);
+            }
+            PopulateComboBox();
+        }
+
+        private void PopulateClassDetails()
+        {
+            // Initialise a list of School Classes and fill with data if available
+            List<Class> classes = new List<Class>();
+            foreach (var schoolClass in smaDB.Classes)
+            {
+                classes.Add(schoolClass);
+            }
+        }
+
+        private void PopulateGuardianDetails()
+        {
+            // Initialise a list of Guardians and fill with data if available
+            List<Guardian> guardians = new List<Guardian>();
+            foreach (var guardian in smaDB.Guardians)
+            {
+                guardians.Add(guardian);
+            }
+        }
+
+        private void PopulateTeacherDetails()
+        {
+            // Initialise a list of Teachers and fill with data if available
+            List<Teacher> teachers = new List<Teacher>();
+            foreach (var teacher in smaDB.Teachers)
+            {
+                teachers.Add(teacher);
+            }
+        }
+
+        private void PopulateAbsenceDetails()
+        {
+            // Initialise a list of AbsenceTypes and fill with data if available
+            List<AbsenceType> absenceTypes = new List<AbsenceType>();
+            foreach (var absenceType in smaDB.AbsenceTypes)
+            {
+                absenceTypes.Add(absenceType);
+            }
+        }
+
+        private void PopulatePupilDetails()
         {
             // Initialise a list of Pupils and fill with data if available
             List<Pupil> pupils = new List<Pupil>();
@@ -236,39 +364,10 @@ namespace SchoolAbsenceMonitorUI
             {
                 pupils.Add(pupil);
             }
-            int count = pupils.Count;
-            // Initialise a list of Teachers and fill with data if available
-            List<Teacher> teachers = new List<Teacher>();
-            foreach (var teacher in smaDB.Teachers)
-            {
-                teachers.Add(teacher);
-            }
-            // Initialise a list of SystemUsers and fill with data if available
-            List<SystemUser> systemUsers = new List<SystemUser>();
-            foreach (var systemUser in smaDB.SystemUsers)
-            {
-                systemUsers.Add(systemUser);
-            }
-            // Initialise a list of Guardians and fill with data if available
-            List<Guardian> guardians = new List<Guardian>();
-            foreach (var guardian in smaDB.Guardians)
-            {
-                guardians.Add(guardian);
-            }
-            int guardiancount = guardians.Count;
-            // Initialise a list of School Classes and fill with data if available
-            List<Class> classes = new List<Class>();
-            foreach (var schoolClass in smaDB.Classes)
-            {
-                classes.Add(schoolClass);
-            }
-            // Initialise a list of AbsenceTypes and fill with data if available
-            List<AbsenceType> absenceTypes = new List<AbsenceType>();
-            foreach (var absenceType in smaDB.AbsenceTypes)
-            {
-                absenceTypes.Add(absenceType);
-            }
-            
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        { 
 
         }
         public List<AccessLevel> AccessLevels {get;set;}
@@ -300,15 +399,120 @@ namespace SchoolAbsenceMonitorUI
                 {
                     // Make the ErrorLabel visible  
                     Lbl_UserAddErrorLabel.Visibility = Visibility.Visible;
+                    // Clear the details from the form
                     TbxGiven.Clear();
                     TbxSurname.Clear();
                     TbxUserId.Clear();
                     TbxPassword.Clear();
                 }
                
+            }            
+        }
+
+        private void BtnUpdateUser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Lbl_UpdateUserErrorLabel.Visibility = Visibility.Hidden;
+                int selectAccessLevel = Convert.ToInt16(CmbBxUpdateAdminLevel.SelectedValue.ToString());
+                userUtils.UpdateUserDetails(new SystemUser
+                {
+                    GivenName = TbxUpdateUserGiven.Text,
+                    Surname = TbxUpdateUserSurname.Text,
+                    Username = TbxUpdateUserName.Text,
+                    Password = TbxUpdatePassword.Text,
+                    AccessLevelId = selectAccessLevel,
+                    UserId = Convert.ToInt16(TbxUpdateUserID.Text)
+                });
+
+                UpdateFormReload(Convert.ToInt16(TbxUpdateUserID.Text));
+            }
+            catch (Exception)
+            {
+                Lbl_UpdateUserErrorLabel.Visibility = Visibility.Visible;
+                Lbl_UpdateUserErrorLabel.Content = "Select an Access Level from the dropdown";
+            }
+        }
+
+        private void BtnReturn_Click(object sender, RoutedEventArgs e)
+        {
+            Stk_UpdateUserForm.Visibility = Visibility.Hidden;
+            ReloadUserSearch("Update");
+            Stk_SearchUser.Visibility = Visibility.Visible;
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ReloadUserSearch("Update");
+        }
+
+        private void DeleteFormReset()
+        {
+            TbxDeleteUserGiven.Clear();
+            TbxDeleteUserSurname.Clear();
+            TbxDeleteUserName.Clear();
+            TbxDeleteUserId.Clear();
+            BtnDeleteUser.Visibility = Visibility.Visible;
+            BtnDeleteUserCancel.Visibility = Visibility.Visible;
+            BtnDeleteUserReturn.Visibility = Visibility.Collapsed;
+            Lbl_UserDeleteSuccessLabel.Visibility = Visibility.Collapsed;
+            Lbl_UserDeleteErrorLabel.Visibility = Visibility.Collapsed;
+            Stk_DeleteUserForm.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {           
+
+            bool confirmDelete = MessageBox.Show("This action cannot be undone","Confirm Deletion", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK;
+
+            if (confirmDelete)
+            {
+                try
+                {
+                    userUtils.DeleteSystemUser(Convert.ToInt16(TbxDeleteUserId.Text.ToString()));
+                    BtnDeleteUser.Visibility = Visibility.Collapsed;
+                    BtnDeleteUserCancel.Visibility = Visibility.Collapsed;
+                    BtnDeleteUserReturn.Visibility = Visibility.Visible;
+                    Lbl_UserDeleteSuccessLabel.Visibility = Visibility.Visible;
+                }
+                catch (Exception)
+                {
+                    Lbl_UserDeleteErrorLabel.Visibility = Visibility.Visible;
+                }                
+            }
+            else
+            {
+                BtnDeleteUser.Visibility = Visibility.Collapsed;
+                BtnDeleteUserCancel.Visibility = Visibility.Collapsed;
+                BtnDeleteUserReturn.Visibility = Visibility.Visible;
+                Lbl_UserDeleteSuccessLabel.Content = "User Deletion Cancelled";
+                Lbl_UserDeleteSuccessLabel.Visibility = Visibility.Visible;
+            }            
+        }
+
+        private void ReloadUserSearch(string formName)
+        {
+            if (formName == "Delete")
+            {
+                DeleteFormReset();
+            }
+            else
+            {
+                UpdateFormReset();
             }
 
-            
+            PopulateUserDetails();
+            Stk_SearchUser.Visibility = Visibility.Visible;
+        }
+
+        private void BtnDeleteUserCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ReloadUserSearch("Delete");
+        }
+
+        private void BtnDeleteUserReturn_Click(object sender, RoutedEventArgs e)
+        {
+            ReloadUserSearch("Delete");
         }
     }
 }
