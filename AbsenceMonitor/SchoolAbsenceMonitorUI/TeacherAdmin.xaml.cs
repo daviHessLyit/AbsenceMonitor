5152bda70c733769edc69d71ed077ce912be2f03
David Hession <L00069108@student.lyit.ie>
2018-12-02 14:07:16 +0000

    Added CRUD options for Teacher.

--- AbsenceMonitor/SchoolAbsenceMonitorUI/TeacherAdmin.xaml.cs
+++ AbsenceMonitor/SchoolAbsenceMonitorUI/TeacherAdmin.xaml.cs
@@ -39,6 +39,16 @@ namespace SchoolAbsenceMonitorUI
             {
                 teachers.Add(teacher);
             }
+
+            PopulateCombo();
+        }
+
+        public List<Class> ClassList { get; set; }
+
+        private void PopulateCombo()
+        {
+            ClassList = smaDB.Classes.ToList();
+            DataContext = ClassList;
         }
 
         private void RefreshTeacherList()
@@ -59,8 +69,8 @@ namespace SchoolAbsenceMonitorUI
                 {
                     GivenName = TbxTeacherGiven.Text.ToString(),
                     Surname = TbxTeacherSurname.Text.ToString(),
-                    ClassId = Convert.ToUInt16(TbxTeacherClassID.Text.ToString())
-                    
+                    ClassId = Convert.ToUInt16(CmbBxClassSelector.SelectedValue.ToString())
+
                 });
 
                 if (teacherAddedSuccess == 1)
@@ -69,7 +79,6 @@ namespace SchoolAbsenceMonitorUI
                     BtnAddCancel.Visibility = Visibility.Collapsed;
                     TbxTeacherGiven.IsEnabled = false;
                     TbxTeacherSurname.IsEnabled = false;
-                    TbxTeacherClassID.IsEnabled = false;
                     Lbl_TeacherLabel.Content = "New Teacher Added Successfully";
                     BtnAddReturn.Visibility = Visibility.Visible;
 
@@ -124,7 +133,6 @@ namespace SchoolAbsenceMonitorUI
             BtnAddCancel.Visibility = Visibility.Visible;            
             TbxTeacherGiven.Text = "";
             TbxTeacherSurname.Text = "";
-            TbxTeacherClassID.Text = "";
             RefreshTeacherList();
             Stk_SearchTeacher.Visibility = Visibility.Visible;
         }
@@ -137,10 +145,207 @@ namespace SchoolAbsenceMonitorUI
             BtnAddReturn.Visibility = Visibility.Visible;
             TbxTeacherGiven.IsEnabled = true;
             TbxTeacherSurname.IsEnabled = true;
-            TbxTeacherClassID.IsEnabled = true;
             TbxTeacherGiven.Text = "";
             TbxTeacherSurname.Text = "";
-            TbxTeacherClassID.Text = "";
+            RefreshTeacherList();
+            Stk_SearchTeacher.Visibility = Visibility.Visible;
+        }
+
+        private void BtnDeleteTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            bool confirmDelete = MessageBox.Show("This action cannot be undone", "Confirm Deletion", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK;
+
+            if (confirmDelete)
+            {
+                int teacherDeleted = teacherUtils.DeleteTeacher(Convert.ToInt16(TbxDeleteTeacherID.Text.ToString()));
+
+                if (teacherDeleted == 1)
+                {
+                    BtnDeleteTeacher.Visibility = Visibility.Collapsed;
+                    BtnDeleteCancel.Visibility = Visibility.Collapsed;
+                    Lbl_DeleteTeacherLabel.Content = "Teacher Successfully Deleted";
+                    BtnDeleteReturn.Visibility = Visibility.Visible;
+                    try
+                    {
+                        systemEventUtils.AddSystemEvent(new SystemEvent
+                        {
+                            UserId = systemUser.UserId,
+                            EventTypeId = 6,
+                            EventDateTime = DateTime.Now,
+                            EventData = $"Teacher record deleted at { DateTime.Now} , by {systemUser.Username}"
+                        });
+                    }
+                    catch (EntityException)
+                    {
+                        MessageBox.Show("System Database Error, Please contact the System Administrator", "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    BtnDeleteTeacher.Visibility = Visibility.Collapsed;
+                    BtnDeleteCancel.Visibility = Visibility.Collapsed;
+                    Lbl_TeacherDeleteErrorLabel.Visibility = Visibility.Visible;
+                    BtnDeleteReturn.Visibility = Visibility.Visible;
+                    try
+                    {
+                        systemEventUtils.AddSystemEvent(new SystemEvent
+                        {
+                            UserId = systemUser.UserId,
+                            EventTypeId = 1006,
+                            EventDateTime = DateTime.Now,
+                            EventData = $"Error deleting Teacher record { DateTime.Now} , by {systemUser.Username}"
+                        });
+                    }
+                    catch (EntityException)
+                    {
+                        MessageBox.Show("System Database Error, Please contact the System Administrator", "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                    }
+                }
+
+            }
+            else
+            {
+                BtnDeleteTeacher.Visibility = Visibility.Collapsed;
+                BtnDeleteCancel.Visibility = Visibility.Collapsed;
+                BtnDeleteReturn.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void BtnDeleteCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Stk_DeleteTeacher.Visibility = Visibility.Hidden;
+            TbxDeleteClassID.Text = "";
+            TbxDeleteTeacherGiven.Text = "";
+            TbxDeleteTeacherSurname.Text = "";
+            TbxDeleteTeacherID.Text = "";
+            RefreshTeacherList();
+            Stk_SearchTeacher.Visibility = Visibility.Visible;
+        }
+
+        private void BtnDeleteReturn_Click(object sender, RoutedEventArgs e)
+        {
+            Stk_DeleteTeacher.Visibility = Visibility.Hidden;
+            BtnDeleteCancel.Visibility = Visibility.Visible;
+            BtnDeleteTeacher.Visibility = Visibility.Visible;
+            BtnDeleteReturn.Visibility = Visibility.Collapsed;
+            TbxDeleteClassID.Text = "";
+            TbxDeleteTeacherGiven.Text = "";
+            TbxDeleteTeacherSurname.Text = "";
+            TbxDeleteTeacherID.Text = "";
+            RefreshTeacherList();
+            Stk_SearchTeacher.Visibility = Visibility.Visible;
+        }
+
+        private void MnuIDeleteTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            Stk_DeleteTeacher.Visibility = Visibility.Visible;
+            var selectedTeacher = teacherUtils.GetTeacher(Convert.ToInt16(LstTeacherSearch.SelectedValue.ToString()));
+            TbxDeleteClassID.Text = selectedTeacher.ClassId.ToString();
+            TbxDeleteTeacherGiven.Text = selectedTeacher.GivenName.ToString();
+            TbxDeleteTeacherSurname.Text = selectedTeacher.Surname.ToString();
+            TbxDeleteTeacherID.Text = selectedTeacher.TeacherId.ToString();
+            Stk_SearchTeacher.Visibility = Visibility.Hidden;
+        }
+
+        private void MnuIUpdateTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            Stk_UpdateTeacher.Visibility = Visibility.Visible;
+            var selectedTeacher = teacherUtils.GetTeacher(Convert.ToInt16(LstTeacherSearch.SelectedValue.ToString()));
+            TbxUpdateTeacherGiven.Text= selectedTeacher.GivenName.ToString();
+            TbxUpdateTeacherSurname.Text = selectedTeacher.Surname.ToString();
+            TbxUpdateTeacherID.Text = selectedTeacher.TeacherId.ToString();
+            Stk_SearchTeacher.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnUpdateTeacher_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int teacherUpdated = teacherUtils.UpdateTeacher(new Teacher
+                {
+                    TeacherId = Convert.ToInt16(TbxUpdateTeacherID.Text.ToString()),
+                    GivenName = TbxUpdateTeacherGiven.Text.ToString(),
+                    Surname = TbxUpdateTeacherSurname.Text.ToString(),
+                    ClassId = Convert.ToInt16(CmbBxClassUpdateSelector.SelectedValue.ToString())
+                });
+
+                if (teacherUpdated == 1)
+                {
+                    BtnUpdateCancel.Visibility = Visibility.Collapsed;
+                    BtnUpdateTeacher.Visibility = Visibility.Collapsed;
+                    BtnUpdateReturn.Visibility = Visibility.Visible;
+                    TbxUpdateTeacherGiven.IsEnabled = false;
+                    TbxUpdateTeacherSurname.IsEnabled = false;
+                    TbxUpdateTeacherID.IsEnabled = false;
+                    CmbBxClassUpdateSelector.IsEnabled = false;
+                    Lbl_UpdateTeacherLabel.Content = "Teacher Successfully Updated";
+
+
+                    try
+                    {
+                        systemEventUtils.AddSystemEvent(new SystemEvent
+                        {
+                            UserId = systemUser.UserId,
+                            EventTypeId = 4,
+                            EventDateTime = DateTime.Now,
+                            EventData = $"Teacher record updated at { DateTime.Now} , by {systemUser.Username}"
+                        });
+                    }
+                    catch (EntityException)
+                    {
+                        MessageBox.Show("System Database Error, Please contact the System Administrator", "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    BtnUpdateCancel.Visibility = Visibility.Collapsed;
+                    BtnUpdateTeacher.Visibility = Visibility.Collapsed;
+                    BtnUpdateReturn.Visibility = Visibility.Visible;
+                    Lbl_TeacherUpdateErrorLabel.Visibility = Visibility.Visible;
+
+                    try
+                    {
+                        systemEventUtils.AddSystemEvent(new SystemEvent
+                        {
+                            UserId = systemUser.UserId,
+                            EventTypeId = 1004,
+                            EventDateTime = DateTime.Now,
+                            EventData = $"Teacher record update error at { DateTime.Now} , by {systemUser.Username}"
+                        });
+                    }
+                    catch (EntityException)
+                    {
+                        MessageBox.Show("System Database Error, Please contact the System Administrator", "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            catch (EntityException)
+            {
+                MessageBox.Show("System Database Error, Please contact the System Administrator", "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnUpdateCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Stk_UpdateTeacher.Visibility = Visibility.Hidden;
+            TbxUpdateTeacherGiven.Text = "";
+            TbxUpdateTeacherSurname.Text = "";
+            TbxUpdateTeacherID.Text = "";
+            RefreshTeacherList();
+            Stk_SearchTeacher.Visibility = Visibility.Visible;
+        }
+
+        private void BtnUpdateReturn_Click(object sender, RoutedEventArgs e)
+        {
+            Stk_UpdateTeacher.Visibility = Visibility.Hidden;
+            TbxUpdateTeacherGiven.Text = "";
+            TbxUpdateTeacherSurname.Text = "";
+            TbxUpdateTeacherID.Text = "";
+            TbxUpdateTeacherGiven.IsEnabled = true;
+            TbxUpdateTeacherSurname.IsEnabled = true;
+            TbxUpdateTeacherID.IsEnabled = true;
+            CmbBxClassUpdateSelector.IsEnabled = true;
             RefreshTeacherList();
             Stk_SearchTeacher.Visibility = Visibility.Visible;
         }
